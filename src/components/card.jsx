adae37487b9aa6a6800a57856f462466c60c201b
meranamnanihai <153189635+meranamnanihai@users.noreply.github.com>
2024-02-21 07:37:07 +0530

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useParams } from 'react-router-dom';
-import Fallback from './fallback';
-
-const ValentineCard = () => {
+import Fallback from './fallback'
+const ValentineCard =  () => {
   const params = useParams();
+  
   const [isOpen, setIsOpen] = useState(false);
-  const partnerName = params.name.split('$')[0];
-  const yourName = params.name.split('$')[1];
-
-  if (partnerName.toLowerCase() === 'admya' || yourName.toLowerCase() === 'admya') {
+  const patnername = params.name.split('$')[0];
+  const yourname = params.name.split('$')[1];
+  
+  if ((patnername.toLowerCase() === 'admya') || (yourname.toLowerCase() === 'admya')) {
     return <Fallback />;
   }
-
+  else{
   return (
     <div className="min-h-screen flex items-center justify-center bg-pink-100">
       <button
@@ -38,20 +38,21 @@ const ValentineCard = () => {
               transition={{ type: 'spring', stiffness: 120, damping: 20 }}
               className="bg-white p-8 rounded-lg shadow-lg max-w-md"
             >
-              <h1 className="text-xl font-bold mb-4">Happy Valentine's Day! Dear {partnerName}</h1>
-              <p className="mb-4">You're the love of my life, Dear {partnerName}</p>
-              <p className="mb-4">My heart beats for you, Dear {partnerName}</p>
-              <p className="mb-4">And I'm grateful for every moment with you.</p>
+              <h1 className="text-xl font-bold mb-4">Happy Valentine's Day! Dear {patnername}</h1>
+              <p className="mb-4">You're the love of my life, Dear {patnername}</p>
+              <p className="mb-4">my heart beats for you, Dear {patnername}</p>
+              <p className="mb-4">and I'm grateful for every moment with you.</p>
               <p className="mb-4">I love you more than words can express.</p>
-              <p className="mb-4">Will you marry me {partnerName}?</p>
+              <p className="mb-4">Will you marry me {patnername}</p>
+            
               <p className="font-semibold">Forever yours,</p>
-              <p className="font-semibold">{yourName}</p>
+              <p className="font-semibold">{yourname}</p>
             </motion.div>
           </motion.div>
         )}
       </AnimatePresence>
     </div>
-  );
+  );}
 };
 
 export default ValentineCard;
